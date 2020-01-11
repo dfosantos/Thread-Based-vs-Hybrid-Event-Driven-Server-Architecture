12ed9f6d761d972299bb56de7196be4e85bc83ef
Diogo <santosdiogo97@gmail.com>
2020-01-11 20:59:59 +0000

    Added ClientSimulator with JPanel

--- Thread Based vs Hybrid Event-Driven Server/src/ClientCreator.java
+++ Thread Based vs Hybrid Event-Driven Server/src/ClientCreator.java
@@ -1,14 +1,51 @@
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class ClientCreator {
 
+	static int ConnectionsPerSecond;
+	static InetAddress ip;
+	static int port;
 	public static void main(String[] args) {
-		
-		if (args.length != 2 ) {
+
+		if (args.length != 2) {
 			System.out.println(
 					"Proper Usage:\n 1st ARGUMENT: Server IP\n 2nd ARGUMENT: Port number\n\tEx.: java ClientCreator 192.168.1.125 12345");
 			System.exit(0);
 		}
+		
+		try {
+			ip = InetAddress.getByName(args[0]);
+		} catch (UnknownHostException e1) {
+			e1.printStackTrace();
+			System.exit(0);
+		}
+		
+		port = Integer.parseInt(args[1]) ;
+		Statistics stats = new Statistics();
+		
+		new KeyListener(stats).run();
+
+		while (true) {
+			
+			Socket s = new Socket();
+			
+			
+			
+			try {
+				Thread.sleep(1000/stats.ConnectionsPerSecond);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(stats.ConnectionsPerSecond);
+		}
 
 	}
+	
+    public static class Statistics {
+        public int ConnectionsPerSecond = 1;
+    }
 
 }
---
+++ Thread Based vs Hybrid Event-Driven Server/src/KeyListener.java
@@ -0,0 +1,73 @@
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+
+public class KeyListener implements Runnable {
+
+	ClientCreator.Statistics cc;
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	public void run() {
+		try {
+			initialize();
+			frame.setVisible(true);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public KeyListener(ClientCreator.Statistics cc) {
+		this.cc = cc;
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 270, 100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblConnectionsPerSecond = DefaultComponentFactory.getInstance().createTitle("Connections Per Second:");
+		frame.getContentPane().add(lblConnectionsPerSecond, BorderLayout.WEST);
+		
+		Action action = new AbstractAction()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				int cps = Integer.parseInt(textField.getText());
+				if(cps != 0)
+					cc.ConnectionsPerSecond = cps;
+				
+			}
+		};
+		textField = new JTextField();
+		textField.addActionListener(action);
+		frame.getContentPane().add(textField, BorderLayout.CENTER);
+		textField.setColumns(10);
+	}
+
+}
